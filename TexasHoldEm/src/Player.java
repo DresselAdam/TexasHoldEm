38f3bfdbef53e9f62fc4e3f88efc4f257aa4b957
Adam Dressel <43581632+DresselAdam@users.noreply.github.com>
2019-04-11 14:35:34 -0400

    Update Player.java

--- TexasHoldEm/src/Player.java
+++ TexasHoldEm/src/Player.java
@@ -1,10 +1,20 @@
+//Adam Dressel
+public class Player extends Board {
+  private static int playerCount;
+  private static int cardCount;
 
-public class Player extends Board{
-	private static int playerCount;
+  // Empty constructor
+  public Player() {
+    playerCount++;
+  }
 
-	// Empty constructor
-	public Player() {
-		playerCount++;
-	}
+  // Creates addCard method that calls the same method for the board class.
+  public void addCard(Card card) {
+    super.addCard(card);
+  }
+
+  public static int getPlayerCount() {
+    return playerCount;
+  }
 }
 
