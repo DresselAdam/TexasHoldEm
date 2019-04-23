29e49ff18d9b59ca1f09d0a5e9b9819d99917269
Adam Dressel <43581632+DresselAdam@users.noreply.github.com>
2019-04-23 11:33:51 -0400

    Update Board.java

--- TexasHoldEm/src/Board.java
+++ TexasHoldEm/src/Board.java
@@ -1,3 +1,4 @@
+
 // Adam Dressel
 import java.util.ArrayList;
 
@@ -9,20 +10,31 @@ public class Board {
 
   }
 
-  // Board constructor with three card arguments, which is the initial size of a board in
-  // TexasHoldem
+  
+  /**Board constructor with three card arguments, 
+   * which is the initial size of a board in TexasHoldem.
+   * @param firstCard First Card on the board
+   * @param secondCard Second card on the board
+   * @param thirdCard Third card on the board.
+   */
   public Board(Card firstCard, Card secondCard, Card thirdCard) {
     board.add(firstCard);
     board.add(secondCard);
     board.add(thirdCard);
   }
 
-  // Gets the entire board ArrayList, useful for getting size of the board and other values.
+  // Gets the entire board ArrayList, useful for getting size of the board and
+  // other values.
   public ArrayList<Card> getBoard() {
     return board;
   }
 
-  // Getting a card at the specified position in the board ArrayList. Useful for card comparisons.
+  public static void setCardCount() {
+    cardCount++;
+  }
+
+  // Getting a card at the specified position in the board ArrayList. Useful for
+  // card comparisons.
   public Card getCard(int boardPos) {
     return board.get(boardPos);
   }
@@ -31,10 +43,11 @@ public class Board {
   // Each time a card is added
   public void addCard(Card nextCard) {
     board.add(nextCard);
-    cardCount++;
+    Board.setCardCount();
   }
 
-  // Counts the number of cards that have been played so far in the game. This method is used to
+  // Counts the number of cards that have been played so far in the game. This
+  // method is used to
   // iterate through
   // the shuffleDeck array numbers, no repeating cards.
   public static int getCount() {
